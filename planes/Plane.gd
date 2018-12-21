ff399f184d43db3cafc29e82684e63fa945ad91a
TheBlueGenie <thebluegenieuk@gmail.com>
2018-12-21 13:17:17 +0000

    Begin Player shooting

--- planes/Plane.gd
+++ planes/Plane.gd
@@ -5,6 +5,8 @@ export (int) var max_speed = 150
 export (int) var throttle_inc = 1
 export (int) var num_guns = 1
 export (int) var gun_offset = 0
+export (float) var gun_cooldown = 1
+export (PackedScene) var Bullet
 
 var curr_speed = 0
 var rotation_speed = 1
@@ -14,6 +16,7 @@ var alive = true
 
 func _ready():
 	curr_speed = min_speed
+	$GunTimer.wait_time = gun_cooldown
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -28,3 +31,10 @@ func _physics_process(delta):
 
 func control(delta):
 	pass
+	
+func shoot(delta):
+	pass
+
+func _on_GunTimer_timeout():
+	can_shoot = true
+	
--- planes/Player.gd
+++ planes/Player.gd
@@ -3,20 +3,34 @@ extends "res://planes/Plane.gd"
 func control(delta):
 	var rot_dir = 0
 	
-	if Input.is_action_pressed('Turn Right'):
+	if Input.is_action_pressed('Turn_Right'):
 		rot_dir += 1
-	if Input.is_action_pressed('Turn Left'):
+	if Input.is_action_pressed('Turn_Left'):
 		rot_dir -= 1
 		
 	rotation += rotation_speed * rot_dir * delta
 	
 	velocity = Vector2()
 	var new_speed = curr_speed
-	if Input.is_action_pressed('Throttle Increase'):
+	if Input.is_action_pressed('Throttle_Increase'):
 		new_speed = clamp(curr_speed + throttle_inc,  min_speed, max_speed) 
-	if Input.is_action_pressed('Throttle Decrease'):
+	if Input.is_action_pressed('Throttle_Decrease'):
 		new_speed = clamp(curr_speed - throttle_inc,  min_speed, max_speed) 
 		
 	velocity = Vector2(new_speed, 0).rotated(rotation)
 	
 	curr_speed = new_speed
+	
+	if Input.is_action_just_pressed('Shoot') and can_shoot:
+		shoot(delta)
+		can_shoot = false
+		$GunTimer.start()
+		
+
+func shoot(delta):
+	var plane_center = $Sprite.get_width / 2
+	
+	if num_guns == 1:
+		pass
+	elif num_guns == 2:
+		pass
